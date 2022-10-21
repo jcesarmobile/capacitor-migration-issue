737df34f3cc5e45b1c68f7ff9635f8d88aacb495
jcesarmobile <jcesarmobile@gmail.com>
2022-10-21 17:04:35 +0200

    migrate fixed

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -7,7 +7,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
     registerPlugin(EchoPlugin.class);
+    super.onCreate(savedInstanceState);
   }
 }
